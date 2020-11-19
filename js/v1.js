ebfbd42004ddb498f0fe4f86b81552671a6dca29
richardwurzer <richard.wurzer@htl-ottakring.ac.at>
2020-11-19 09:24:39 +0100

    found v1

--- js/v1.js
+++ js/v1.js
@@ -1,9 +1,10 @@
 const numberCards = 16
+let openedCards = []
 
 let p = document.querySelector('.deck')
 for (let index = 0; index < numberCards; index++) {
         let c = document.createElement('div')
-        c.innerHTML = index + 1
+        c.innerHTML = "Hallo "+ (index + 1)
         c.type = index % 2 + 1
         c.className="card"
         c.addEventListener('click',flipp)
@@ -11,7 +12,12 @@ for (let index = 0; index < numberCards; index++) {
 }
 
 function flipp(event){
-    this.classList.toggle('flipped')
+    if(!this.classList.contains('found'))
+        openCard(this)
+    //this.classList.toggle('flipped')
+}
+function openCard(c){
+    alert(c.innerHTML)
 }
 
 
