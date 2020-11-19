1e0992d156f27078ba91de4bc1452ff2ed85673c
richardwurzer <richard.wurzer@htl-ottakring.ac.at>
2020-11-19 09:13:29 +0100

    favicon

--- js/v1.js
+++ js/v1.js
@@ -3,7 +3,8 @@ const numberCards = 16
 let p = document.querySelector('.deck')
 for (let index = 0; index < numberCards; index++) {
         let c = document.createElement('div')
-        c.innerHTML = index % 2 + 1
+        c.innerHTML = index + 1
+        c.type = index % 2 + 1
         c.className="card"
         c.addEventListener('click',flipp)
         p.appendChild(c)
